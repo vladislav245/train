a6209f3c90cdb68f3ba685999fa4a935cff4a422
vlad <vlad-golovinov@mail.ru>
2023-05-13 16:07:31 +0300

    new_commit_branch

--- my_pr.cpp
+++ my_pr.cpp
@@ -19,55 +19,47 @@ public:
     }
 };
 
+class Man {
+
+
+private:
+    int age;
+    int weight;
+    int height;
+    int width;
+    
+public:
+    Man() { };
+    Man(int n) : age(n), weight(n), height(n), width(n) { };
+    Man(const Man& other) {
+        *this = other;
+    }
+
+    Man& operator = (const Man& other) {
+        this->age = other.age;
+        this->weight = other.weight;
+        this->height = other.height;
+        this->width = other.width;
+        return *this;
+    }
+
+    void get_info() {
+        cout << age << endl;
+        cout << weight << endl;
+        cout << height << endl;
+        cout << width << endl;
+    }
+
+};
+
 
 int main()
 {
-
-    cout << "calcutation" << endl;
-
-    double x, y;
-
-    cout << "write x and y" << endl;
-    cin >> x;
-    cin >> y;
-
-    try {
-        pow(x, y);
-        sqrt(x);
-        sqrt(y);
-    }
-    catch (...) {
-        cout << "it`s problem" << endl;
-    }
-
-    try {
-        sqrt(pow(x, y));
-        sqrt(pow(y, x));
-        pow(sin(x), cos(x));
-        pow(sin(y), cos(y));
-        pow(sin(x), cos(y));
-        pow(sin(y), cos(x));
-        pow(sin(x), sin(x));
-        pow(cos(x), cos(x));
-        pow(sin(y), sin(y));
-        pow(cos(y), cos(y));
-    }
-    catch (...) {
-        cout << "it`s problem" << endl;
-    }
-
-
+    Man arr[3];
+    
+    arr[0] = -1;
+    arr[1] = 9;
+    arr[2] = 6;
 
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
